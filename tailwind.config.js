351bf1e0afd5c121b86e400088b88558aba7a866
Nathan Lawson <nathan@consultanddesign.com>
2020-09-18 19:24:54 +0100

    NuxtJS & initial styles

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'gray-900-spotify': '#121212',
+        'gray-800-spotify': '#181818',
+        'gray-700-spotify': '#282828',
+      }
+    }
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
